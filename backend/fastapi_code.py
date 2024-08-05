07659384b8ac28b82024eb827655d51f4fe17e7d
Parth Mehta <46846270+parthmehta15@users.noreply.github.com>
2024-08-05 12:48:43 -0400

    Update fastapi_code.py

--- backend/fastapi_code.py
+++ backend/fastapi_code.py
@@ -1,4 +1,9 @@
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
